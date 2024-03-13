ce0976c0919ec747d722f0a5927041ba21427988
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2024-03-13 21:51:58 +0200

    Added custom exception classes to handle Different Exceptions scenarios in the API.

---
+++ E-Commerece.API/ExceptionsConfiguration/Exceptions/KeyNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace E_Commerece.API.ExceptionsConfiguration.Exceptions
+{
+    public class KeyNotFoundException : Exception
+    {
+        public KeyNotFoundException(string msg) : base(msg)
+        {
+
+        }
+    }
+
+}
---
+++ E-Commerece.API/ExceptionsConfiguration/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace E_Commerece.API.ExceptionsConfiguration.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string msg ) : base(msg)
+        {
+                
+        }
+    }
+}
