5fb7092484c98266e6c31c5b5cc8c5593a0ac925
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2024-04-20 23:53:37 +0200

    Implemented Identity Service Extension: Configured and added IdentityDbContext for user authentication, integrated with ASP.NET Core Identity, and extended ApplicationUser class with custom properties.

---
+++ Core/Identity/Address.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Identity
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string FName { get; set; } = string.Empty;
+        public string LName {  get; set; } = string.Empty;
+        public string Street { get; set; }= string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Governorate { get; set; } = string.Empty;
+        public string ZipCode { get; set; } = string.Empty;
+
+
+        // Navigation Properitites
+        public ApplicationUser User { get; set; }
+        [Required]
+        public string AppUserId { get; set; }
+
+    }
+}
---
+++ E-Commerece.API/Extensions/IdentityExtensions/IdentityServiceExtension.cs
@@ -0,0 +1,20 @@
+﻿using E_Commerece.API.ExceptionsConfiguration;
+using InfraStructure.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerece.API.Extensions.IdentityExtensions
+{
+    public static class IdentityServiceExtension
+    {
+        public static void AddIdentityService(this IServiceCollection services , IConfiguration configuration)
+        {
+            services.AddDbContext<AppIdentityDbContext>
+            (options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("IdentityConnection"));
+
+            });
+
+        }
+    }
+}
