b190a72791bcd4fc8d9e8519a5baed78da20e075
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2024-04-20 23:13:23 +0200

    Revamped basket functionality: Added BasketItem properties and improved CustomerBasket structure for enhanced shopping experience

---
+++ Core/Models/BasketItem.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.Models
+{
+    public class BasketItem
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public Decimal? Price { get; set; }
+        public int Quantity { get; set; }
+        public string PictureUrl { get; set; } = string.Empty;
+        public string Brand { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+
+
+    }
+}
