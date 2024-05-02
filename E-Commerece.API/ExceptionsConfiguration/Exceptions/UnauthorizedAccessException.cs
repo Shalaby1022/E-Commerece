da335282214dead45d70d0d9d286a95fcae77b7d
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2024-05-02 16:25:53 +0300

    Add ITokenService interface for token creation and update configuration settings

--- E-Commerece.API/ExceptionsConfiguration/Exceptions/UnauthorizedAccessException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace E_Commerece.API.ExceptionsConfiguration.Exceptions
-{
-    public class UnauthorizedAccessException : Exception
-    {
-        public UnauthorizedAccessException(string msg) : base(msg)
-        {
-        }
-    }
-}
---
+++ InfraStructure/Services/TokenService.cs
@@ -0,0 +1,57 @@
+﻿using Core.Identity;
+using Core.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Client.Platforms.Features.DesktopOs.Kerberos;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfraStructure.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly SymmetricSecurityKey _symmetricSecurityKey; 
+
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Key"]));    
+
+        }
+        public string CreateToken(ApplicationUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.GivenName , user.UserName),
+
+            };
+
+            var credeitntials = new SigningCredentials(_symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = credeitntials,
+                Issuer = _configuration["JWT:Issuer"],
+                Audience = _configuration["JWT:Audience"]
+
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+
+        }
+    }
+}
