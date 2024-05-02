6644263ba430d39463379d1b1a3dec10432d13ff
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2024-05-02 16:24:06 +0300

    Implement Identity-related infrastructure changes including DbContext setup, one-to-one relationship configuration, and initial data seeding

--- InfraStructure/Identity/IdentityDbContext.cs
+++ InfraStructure/Identity/IdentityDbContext.cs
@@ -1,6 +1,9 @@
-﻿using InfraStructure.Data;
+﻿using Core.Identity;
+using InfraStructure.Data;
+using InfraStructure.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +12,7 @@ using System.Threading.Tasks;
 
 namespace InfraStructure.Identity
 {
-    public class AppIdentityDbContext : IdentityDbContext
+    public class AppIdentityDbContext : IdentityDbContext<ApplicationUser>
     {
         public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
         {
@@ -17,6 +20,11 @@ namespace InfraStructure.Identity
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+            builder.Entity<ApplicationUser>()
+                .HasOne(x => x.Address)
+                .WithOne()
+                .HasForeignKey<Address>(x => x.AppUserId)
+                .OnDelete(DeleteBehavior.Restrict);
         }
 
     }
---
+++ InfraStructure/Identity/IdentityDbContextSeed.cs
@@ -0,0 +1,46 @@
+﻿using Core.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfraStructure.Identity
+{
+    public class IdentityDbContextSeed
+    {
+
+        private readonly ModelBuilder _builder;
+
+        public IdentityDbContextSeed(ModelBuilder modelBuilder)
+        {
+            _builder = modelBuilder;
+        }
+        public static async Task SeddingIdentity(UserManager<ApplicationUser> manager)
+        {
+            if (!manager.Users.Any())
+            {
+                var user = new ApplicationUser
+                {
+                    Email = "Shelby@2002.com",
+                    UserName = "Shelby",
+                    Address = new Address
+                    {
+                        FName = "Ahmed",
+                        LName = "Shelby",
+                        Street = "10st",
+                        City = "Mnf",
+                        Governorate = "Menofia",
+                        ZipCode = "32951"
+                    }
+
+                };
+
+
+                await manager.CreateAsync(user, "Shelby#@142002");
+            }
+        }
+    }
+}
